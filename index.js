af0cad9aba257ada00cbf6e561c819df5d25e78a
AritraStark <58631915+AritraStark@users.noreply.github.com>
2020-08-19 20:48:16 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,65 @@
+function myFunction1() {
+  var x = document.querySelector(".s1");
+  var y = document.querySelector(".s2");
+  var z = document.querySelector(".s3");
+  var i = document.querySelector(".s4");
+  var j = document.querySelector(".s5");
+  x.style.display = "grid";
+  x.style.zIndex=500;
+  y.style.display = "none";
+  z.style.display = "none";
+  i.style.display = "none";
+  j.style.display = "none";
+}
+function myFunction2() {
+  var x = document.querySelector(".s1");
+  var y = document.querySelector(".s2");
+  var z = document.querySelector(".s3");
+  var i = document.querySelector(".s4");
+  var j = document.querySelector(".s5");
+  y.style.display = "grid";
+  y.style.zIndex=500;
+  x.style.display = "none";
+  z.style.display = "none";
+  i.style.display = "none";
+  j.style.display = "none";
+}
+function myFunction3() {
+  var x = document.querySelector(".s1");
+  var y = document.querySelector(".s2");
+  var z = document.querySelector(".s3");
+  var i = document.querySelector(".s4");
+  var j = document.querySelector(".s5");
+  z.style.display = "grid";
+  z.style.zIndex=500;
+  y.style.display = "none";
+  x.style.display = "none";
+  i.style.display = "none";
+  j.style.display = "none";
+}
+function myFunction4() {
+  var x = document.querySelector(".s1");
+  var y = document.querySelector(".s2");
+  var z = document.querySelector(".s3");
+  var i = document.querySelector(".s4");
+  var j = document.querySelector(".s5");
+  i.style.display = "grid";
+  i.style.zIndex=500;
+  y.style.display = "none";
+  z.style.display = "none";
+  x.style.display = "none";
+  j.style.display = "none";
+}
+function myFunction5() {
+  var x = document.querySelector(".s1");
+  var y = document.querySelector(".s2");
+  var z = document.querySelector(".s3");
+  var i = document.querySelector(".s4");
+  var j = document.querySelector(".s5");
+  j.style.display = "grid";
+  j.style.zIndex=500;
+  y.style.display = "none";
+  z.style.display = "none";
+  i.style.display = "none";
+  x.style.display = "none";
+}
